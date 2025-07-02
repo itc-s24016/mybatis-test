8a1deb0df139b8bb4dd90dfdbac796397f5706a8
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-02 14:48:29 +0900

    主キーで検索

--- src/main/kotlin/select/SelectTest1.kt
+++ src/main/kotlin/select/SelectTest1.kt
@@ -1,5 +1,5 @@
 package select
-//primary key が返す値は0, 1件
+//主キーで検索
 import common.createSessionFactory
 import database.UserMapper
 import database.selectByPrimaryKey
@@ -7,8 +7,9 @@ import database.selectByPrimaryKey
 fun main() {
     createSessionFactory().openSession().use { session ->
         val mapper = session.getMapper(UserMapper::class.java)
+
+        //primary key が返す値は0, 1件
         val user = mapper.selectByPrimaryKey(100)
         println(user)
-        //主キーの値が
     }
 }
